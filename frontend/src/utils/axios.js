7045a88b0cd8e33d399e5d022fa4db92dc2b6349
Hitesh <hiteshkumarnain@gmail.com>
2026-02-24 08:15:19 +0530

    axios frontend

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  // baseURL: "",
-  baseURL: "http://localhost:5000/api/",
+  baseURL: "https://tnp-iiits.onrender.com/api/",
+  // baseURL: "http://localhost:5000/api/",
   withCredentials: true, 
 });
 
